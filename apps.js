563ce794078c9d67acdbed70aaa0c3b63bc0922b
Carlosff02 <131286690+Carlosff02@users.noreply.github.com>
2024-06-23 12:31:38 -0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,10 @@
+/*
+Operaciones con arreglos
+array.push(5); agrega un elemento al ultimo elemento del arreglo
+array.length; tamaño del arreglo
+array[array.length-1] ultimo elemento
+array[0] primer elemento
+array.includes(elemento) retorna true si existe el elemento en el arreglo
+false si no existe en el arreglo
+array.pop() elimina el último elemento
+*/
